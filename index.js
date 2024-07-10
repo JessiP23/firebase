d80da82b025e9a9b8252b6a46c190d3cef6ab441
Jessi Pavia <jessi316866@gmail.com>
2024-07-10 15:06:22 -0400

    communication between the firebase database and our project

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import { add } from "./function.js";
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+
+// Challenge: Make sure that add is working by console logging. add with two number arguments.
+console.log(add(1,1));
+
 const appSettings = {
     databaseURL: 'https://overview-1eaad-default-rtdb.firebaseio.com/'
 }
+
+const app = initializeApp(appSettings);
+
+console.log(app);
 
 // Console firebase, Realtime database, create database, select region, and copy the url into the databaseURL
 
