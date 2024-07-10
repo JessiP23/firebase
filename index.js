ea311d8e7dcf60bc61baf5570dcf342fa99a07d0
Jessi Pavia <jessi316866@gmail.com>
2024-07-10 15:35:18 -0400

    list with form cleared after a submission

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import { add } from "./function.js";
 import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 import {getDatabase, ref, push} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
-// Challenge: Make sure that add is working by console logging. add with two number arguments.
-console.log(add(1,1));
 
 const appSettings = {
     databaseURL: 'https://overview-1eaad-default-rtdb.firebaseio.com/'
@@ -11,17 +8,20 @@ const appSettings = {
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app)
-const moviesInDB = ref(database, 'movies')
-
-console.log(app);
+const shoppingListInDB = ref(database, 'shoppingList')
 
 // Console firebase, Realtime database, create database, select region, and copy the url into the databaseURL
 
 const inputForm = document.getElementById('input-field');
 const buttonEvent = document.getElementById("add-button");
+const list = document.getElementById("shopping-list");
 
 buttonEvent.addEventListener("click", function() {
     let inputValue = inputForm.value;
-    push(moviesInDB, inputValue)
-    console.log(inputValue);
+    push(shoppingListInDB, inputValue);
+
+    // Challenge: Clear the input field when button is pressed
+    inputForm.value = ''
+
+    list.innerHTML += `<li>${inputValue}</li>`
 })
