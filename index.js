3f2eed9edad49f2bad9c631911ea10ddf8f1c888
Jessi Pavia <jessi316866@gmail.com>
2024-07-10 15:49:48 -0400

    example to convert Object to an array

--- index.js
+++ index.js
@@ -35,3 +35,21 @@ function clearInputFieldEl() {
 function appendItemToShoppingListEl(itemValue) {
     list.innerHTML += `<li>${itemValue}</li>`
 }
+
+/*
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "per@scrimba.com",
+    "02": "frode@scrimba.com"
+}
+
+
+let scrimbaUsersEmails = Object.values(scrimbaUsers)
+console.log(scrimbaUsersEmails);
+
+let scribaUsersIds = Object.keys(scrimbaUsers);
+console.log(scribaUsersIds);
+
+let scrimbaUsersEntries = Object.entries(scrimbaUsers);
+console.log(scrimbaUsersEntries);
+*/
